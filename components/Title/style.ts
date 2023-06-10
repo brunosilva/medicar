e201e10813b48d992776ec3264ee2481c42af9c4
Bruno Silva <brs.rsilva@gmail.com>
2023-06-10 14:58:29 -0300

    feat: :sparkles: title component

---
+++ components/Title/style.ts
@@ -0,0 +1,15 @@
+import { styled } from "@stitches/react";
+
+export const Title = styled('div', {
+  fontStyle: 'normal',
+  fontWeight: 700,
+  fontSize: '18px',
+  lineHeight: '21px',
+
+  display: 'flex',
+  alignItems: 'center',
+
+  color: '$black',
+
+  marginBottom: 20,
+})
