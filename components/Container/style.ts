cd97b242fb81ee19298e9f9e34be82c5e9630aff
Bruno Silva <brs.rsilva@gmail.com>
2023-06-10 14:57:24 -0300

    feat: :sparkles: container component

---
+++ components/Container/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+export const Container = styled.div`
+  width: 100vw;
+  display: flex;
+  justifyContent: center;
+  alignItems: center;
+  padding: 40px;
+`
+export const Content = styled.div`
+  padding: 40px;
+`
