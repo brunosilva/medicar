7c0d8754483238172c21688d14e18ea5eb313d7f
Bruno Silva <brs.rsilva@gmail.com>
2023-06-10 19:00:26 -0300

    style: :sparkles: create button theme

--- components/Button/index.ts
+++ components/Button/index.ts
@@ -1,8 +1,6 @@
 import { styled } from '../../styles';
 
 export const Button = styled('button', {
-  backgroundColor: '$green300',
-  color: '$white',
   borderRadius: 4,
   border: 0,
   padding: '9px 38px',
@@ -12,8 +10,24 @@ export const Button = styled('button', {
   span: {
     fontWeight: 'bold',
   },
+  variants: {
+    color: {
+      green: {
+        backgroundColor: '$green300',
+        color: '$white',
 
-  '&:hover': {
-    filter: 'brightness(0.8)'
+        '&:hover': {
+          filter: 'brightness(0.8)'
+        }
+      },
+      transparent: {
+        backgroundColor: '$white',
+        color: '$green300',
+
+        '&:hover': {
+          textDecoration: 'underline',
+        }
+      },
+    },
   }
 })
