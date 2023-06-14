97b277f54d97d7e8219d75cf0f51bd51ab672a13
Bruno Silva <brs.rsilva@gmail.com>
2023-06-13 23:31:08 -0300

    refactor: refactor appointments layout

--- pages/appointments/index.tsx
+++ pages/appointments/index.tsx
@@ -1,17 +1,30 @@
+import { useRouter } from "next/router";
 import { Button } from "../../components/Button";
 import Icon from "../../components/Icon";
+import Logo from "../../components/Logo";
 import Table from "../../components/Table";
 import { TitleSection } from "../../components/Title/style";
-import { ContainerAppointments, Row } from "./type";
+import { ContainerAppointments, Content, HeaderAppointments, LoginButtons, Row } from "./type";
 
 export default function Appointments() {
+  const route = useRouter();
+
   return (
     <ContainerAppointments>
-      <Row>
-        <TitleSection><span>Consulta Clínica</span></TitleSection>
-        <Button color="green"><Icon model="plus" />Acessar</Button>
-      </Row>
-      <Table />
+      <HeaderAppointments>
+        <Logo />
+        <LoginButtons>
+          <span>Bruno Silva</span>
+          <Button color="transparent" onClick={() => route.push('/login')}>Desconectar</Button>
+        </LoginButtons>
+      </HeaderAppointments>
+      <Content>
+        <Row>
+          <TitleSection><span>Consulta Clínica</span></TitleSection>
+          <Button color="green" onClick={() => route.push('/new')}><Icon model="plus" />Nova Consulta</Button>
+        </Row>
+        <Table />
+      </Content>
     </ContainerAppointments>
   )
 }
--- pages/appointments/type.ts
+++ pages/appointments/type.ts
@@ -1,7 +1,24 @@
 import { styled } from "@stitches/react";
 
 export const ContainerAppointments = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+export const HeaderAppointments = styled('div', {
+  display: 'flex',
+  justifyContent: 'space-between',
+  marginBottom: '15px',
+})
+
+export const LoginButtons = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+})
+
+export const Content = styled('div', {
   border: '1px solid #A8A8A8',
+  borderRadius: '7px',
 })
 
 export const Row = styled('div', {
@@ -10,4 +27,5 @@ export const Row = styled('div', {
   alignItems: 'center',
 
   marginBottom: '20px',
+  padding: '10px',
 })
