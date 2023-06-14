9779476b53ba5a8c525b8b0af7f7e67fc9b29f3c
Bruno Silva <brs.rsilva@gmail.com>
2023-06-13 21:48:13 -0300

    refactor: :fire: remove useless code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,17 +4,5 @@ import { globalStyles } from "../styles/global";
 globalStyles()
 
 export default function App({Component, pageProps}: AppProps) {
-  return (
-    <>
-      {/* <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/new" element={<NewAppointment/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/success" element={<Success/>}/>
-        <Route path="*" element={<Login/>}/>
-      </Routes> */}
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
