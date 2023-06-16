12822f41973a45ae9b7d930efc322b2a20f9ca19
Bruno Silva <brs.rsilva@gmail.com>
2023-06-15 21:49:40 -0300

    refactor: :art: layout new component

--- pages/new/index.tsx
+++ pages/new/index.tsx
@@ -4,9 +4,12 @@ import { Button } from "../../components/Button";
 import Dropdown from "../../components/Dropdown/indes";
 import Form from "../../components/Form";
 import { Field, FormAction } from "../../components/Form/style";
+import Logo from "../../components/Logo";
+import { RowLogo } from "../style";
 import useGetCalendar from "./hooks/useGetCalendar";
 import useGetDoctors from "./hooks/useGetDoctors";
 import useGetSpecialties from "./hooks/useGetSpecialties";
+import { ContainerNew } from "./style";
 
 export default function NewAppointment() {
   const route = useRouter();
@@ -33,23 +36,28 @@ export default function NewAppointment() {
   }, [])
 
   return (
-    <Form title="Nova consulta">
-      <Field>
-        <Dropdown name="especialidade" placeholder="Especialidade" data={specialties} onChange={handleDoctor} typeValue="nome" />
-      </Field>
-      <Field>
-        <Dropdown name="medico" placeholder="Médico" data={doctors} onChange={((value) => handleDate(value))} typeValue="nome" />
-      </Field>
-      <Field>
-        <Dropdown name="data" placeholder="Data" data={calendar} onChange={((value) => handleDate(value))} typeValue="data" />
-      </Field>
-      <Field>
-        <Dropdown name="hora" placeholder="Hora" data={calendar} onChange={((value) => handleDate(value))} typeValue="hora"  />
-      </Field>
-      <FormAction>
-        <Button color="transparent" onClick={() => route.push('/appointments')}>Cancelar</Button>
-        <Button color="green" onClick={() => route.push('/appointments')}>Confirmar</Button>
-      </FormAction>
-    </Form>
+    <ContainerNew>
+      <RowLogo margin="large">
+        <Logo />
+      </RowLogo>
+      <Form title="Nova consulta">
+        <Field>
+          <Dropdown name="especialidade" placeholder="Especialidade" data={specialties} onChange={handleDoctor} typeValue="nome" />
+        </Field>
+        <Field>
+          <Dropdown name="medico" placeholder="Médico" data={doctors} onChange={((value) => handleDate(value))} typeValue="nome" />
+        </Field>
+        <Field>
+          <Dropdown name="data" placeholder="Data" data={calendar} onChange={((value) => handleDate(value))} typeValue="data" />
+        </Field>
+        <Field>
+          <Dropdown name="hora" placeholder="Hora" data={calendar} onChange={((value) => handleDate(value))} typeValue="hora"  />
+        </Field>
+        <FormAction>
+          <Button color="transparent" onClick={() => route.push('/appointments')}>Cancelar</Button>
+          <Button color="green" onClick={() => route.push('/appointments')}>Confirmar</Button>
+        </FormAction>
+      </Form>
+    </ContainerNew>
   )
 }
---
+++ pages/new/style.ts
@@ -0,0 +1,6 @@
+import { styled } from "@stitches/react";
+
+export const ContainerNew = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+})
