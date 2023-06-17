6b0a6f540978872c8ace34917ab5875e7cd07ae7
Bruno Silva <brs.rsilva@gmail.com>
2023-06-17 14:03:06 -0300

    refactor: :zap: refactor dropdown type and interface

--- components/Dropdown/indes.tsx
+++ components/Dropdown/indes.tsx
@@ -1,5 +1,5 @@
-import { IDoctors, ISpecialties } from "../../pages/new/hooks/type";
 import { Option, Select } from "./style";
+import { IOption } from "./type";
 
 // interface IOptions {
 //   key: string;
@@ -8,18 +8,18 @@ import { Option, Select } from "./style";
 // }
 
 interface IProps {
-  data: ISpecialties[] | IDoctors[];
+  data: IOption[];
   placeholder: string;
   name: string;
   onChange: (id: string) => void;
-  typeValue: 'nome' | 'data' | 'hora';
 }
-export default function Dropdown<IProps>({data, placeholder, name, onChange, typeValue}) {
+export default function Dropdown<IProps>({data, placeholder, name, onChange}) {
   return (
     <Select name={name} placeholder={placeholder} onChange={(e) => onChange(e.target.value)}>
       <Option>{placeholder}</Option>
-      {data?.map((item, index) =>
-        <Option key={index} value={item.id}>{typeValue === 'nome' ? item.nome : typeValue === 'data' ? item.dia : item?.horarios?.map((hora, index) => `${hora}`)}</Option>
+      {data?.map((item) =>
+        <Option key={item?.key} value={item?.value}>{item?.text}</Option>
+        // <Option key={item.key} value={item.value}>{typeValue === 'nome' ? item.nome : typeValue === 'data' ? item.dia : item?.horarios?.map((hora, index) => `${hora}`)}</Option>
       )}
     </Select>
   )
--- pages/new/hooks/type.ts
+++ pages/new/hooks/type.ts
@@ -13,17 +13,23 @@ export interface IDoctors {
   }
 }
 
-export interface ICalendar {
-    id: string,
-    medico: {
-      id: string,
-      crm: number,
-      nome: string,
-      especialidade: {
-          id: string,
-          nome: string
-      }
-    },
-    dia: string,
-    horarios: string[]
+export interface IHours {
+  id: number,
+  value: string
 }
+
+export interface ICalendar {
+  id: string,
+  medico: {
+    id: string,
+    crm: number,
+    nome: string,
+    especialidade: {
+        id: string,
+        nome: string
+    }
+  },
+  dia: {id: number, day: number, value: string},
+  horarios: IHours[]
+}
+
