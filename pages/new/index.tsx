37e1a244f1f767352bb00ba775350b1cd74b6269
Bruno Silva <brs.rsilva@gmail.com>
2023-06-17 14:04:37 -0300

    fix: :zap: fix request from dropdown

--- pages/new/index.tsx
+++ pages/new/index.tsx
@@ -1,11 +1,13 @@
+import moment from "moment";
 import { useRouter } from "next/router";
-import { useCallback } from "react";
+import { useState } from "react";
 import { Button } from "../../components/Button";
 import Dropdown from "../../components/Dropdown/indes";
 import Form from "../../components/Form";
 import { Field, FormAction } from "../../components/Form/style";
 import Logo from "../../components/Logo";
 import { RowLogo } from "../style";
+import { ICalendar, IDoctors } from "./hooks/type";
 import useGetCalendar from "./hooks/useGetCalendar";
 import useGetDoctors from "./hooks/useGetDoctors";
 import useGetSpecialties from "./hooks/useGetSpecialties";
@@ -13,6 +15,10 @@ import { ContainerNew } from "./style";
 
 export default function NewAppointment() {
   const route = useRouter();
+
+  const [specialtiesSelected, setSpecialtiesSelected] = useState("")
+  const [doctorSelected, setDoctorSelected] = useState("")
+  const [dateSelected, setDateSelected] = useState("")
 
   const {
     specialties
@@ -27,13 +33,72 @@ export default function NewAppointment() {
     calendar
   } = useGetCalendar()
 
-  const handleDoctor = useCallback((id) => {
-    console.log('handleDoctor', id)
-  }, [])
+  const formatSpecialties = (data) => {
+    return (
+      data?.map((item) => ({
+        text: item.nome,
+        value: item.nome,
+        key: item.id
+      }))
+    )
+  }
 
-  const handleDate = useCallback((id) => {
-    console.log('handleDate', id)
-  }, [])
+  const formatDoctor = (data) => {
+    return (
+      data?.map((item) => ({
+        text: item.nome,
+        value: item.nome,
+        key: item.id
+      }))
+    )
+  }
+
+  const formatDate = (data) => {
+    return (
+      data?.dia?.map((item) => ({
+        text: moment(item?.value).format("dddd, MMMM Do YYYY"),
+        value: item?.id,
+        key: item?.id
+      }))
+    )
+  }
+
+  // const formatHours = (data) => {
+  //   // console.log("formatCalendar", data)
+
+  //     // return (
+  //       //   data?.map((item) => )
+  //       // )
+  // }
+
+  const handleDoctor = (value) => {
+    const doctorsBySpecialties = doctors?.filter((item: IDoctors) => item?.especialidade?.nome === value)
+    return formatDoctor(doctorsBySpecialties)
+  }
+
+  const handleDate = (value) => {
+    const dateByDoctor = calendar?.find((item: ICalendar) => item?.medico?.nome === value)
+    return formatDate(dateByDoctor)
+  }
+
+  const handleHour = (value) => {
+    console.log("dateSelected", dateSelected);
+    console.log("handleHour", value);
+    // const hourByDate = calendar?.find((item: ICalendar) => item?.medico?.nome === value)
+    // return formatDate(dateByDoctor)
+  }
+
+  // const handleHours = (data) => {
+  //   console.log("handleCalendar", data)
+  //   return (
+  //     data?.horarios?.map((item, index) => ({
+  //       text: item,
+  //       value: index,
+  //       key: index
+  //     }))
+  //   )
+  // }
+
 
   return (
     <ContainerNew>
@@ -42,16 +107,36 @@ export default function NewAppointment() {
       </RowLogo>
       <Form title="Nova consulta">
         <Field>
-          <Dropdown name="especialidade" placeholder="Especialidade" data={specialties} onChange={handleDoctor} typeValue="nome" />
+          <Dropdown
+            name="especialidade"
+            placeholder="Especialidade"
+            data={formatSpecialties(specialties)}
+            onChange={(value: string) => setSpecialtiesSelected(value)}
+          />
         </Field>
         <Field>
-          <Dropdown name="medico" placeholder="Médico" data={doctors} onChange={((value) => handleDate(value))} typeValue="nome" />
+          <Dropdown
+            name="medico"
+            placeholder="Médico"
+            data={handleDoctor(specialtiesSelected)}
+            onChange={((value: string) => setDoctorSelected(value))}
+          />
         </Field>
         <Field>
-          <Dropdown name="data" placeholder="Data" data={calendar} onChange={((value) => handleDate(value))} typeValue="data" />
+          <Dropdown
+            name="data"
+            placeholder="Data"
+            data={handleDate(doctorSelected)}
+            onChange={((value) => setDateSelected(value))}
+          />
         </Field>
         <Field>
-          <Dropdown name="hora" placeholder="Hora" data={calendar} onChange={((value) => handleDate(value))} typeValue="hora"  />
+          <Dropdown
+            name="hora"
+            placeholder="Hora"
+            data={handleHour(dateSelected)}
+            onChange={((value) => handleDate(value))}
+          />
         </Field>
         <FormAction>
           <Button color="transparent" onClick={() => route.push('/appointments')}>Cancelar</Button>
